10d1ea49d918dbf8273423012e7c7a6ad97e9840
karcher <karcher@embl.de>
2021-04-15 18:08:37 +0200

    modified analyse_barcode_distribution.py to consume less memory

--- scripts/analyse_barcode_distribution.py
+++ scripts/analyse_barcode_distribution.py
@@ -19,20 +19,18 @@ readInfo.columns = ["sampleID", "readName", "flag", "alignmentPosition"]
 readInfo = dict(zip(list(readInfo['readName']), list(readInfo['alignmentPosition'])))
 i = 0
 
-for read in fasta:
-    readStr = str(read.seq)
-    # Detect spacer sequence in read
-    if spacerSeq in readStr:
-        # Get starting index of spacer hit
-        spacerStartIndex = readStr.index(spacerSeq)
-        # Sanity check: Verify that we can find the BuniformisIdentifier in the read, i.e. use only those reads..
-        if readStr[(spacerStartIndex-26-3) : (spacerStartIndex-25)] == BuniformisIdentifer:
-            BarcodeSequence = readStr[(spacerStartIndex-25) : spacerStartIndex]
-            readsWithBarcodes.append([sampleID, read.id, readStr, readInfo[read.id], BarcodeSequence])
-
 with open(pathToParsedReadFile, "w") as f:
-  for i, entry in enumerate(readsWithBarcodes):
-    f.write("{}\t{}\t{}\t{}\t{}\n".format(*entry))
+    for read in fasta:
+        readStr = str(read.seq)
+        # Detect spacer sequence in read
+        if spacerSeq in readStr:
+            # Get starting index of spacer hit
+            spacerStartIndex = readStr.index(spacerSeq)
+            # Sanity check: Verify that we can find the BuniformisIdentifier in the read, i.e. use only those reads..
+            if readStr[(spacerStartIndex-26-3) : (spacerStartIndex-25)] == BuniformisIdentifer:
+                BarcodeSequence = readStr[(spacerStartIndex-25) : spacerStartIndex]
+                #readsWithBarcodes.append([sampleID, read.id, readStr, readInfo[read.id], BarcodeSequence])
+                f.write("{}\t{}\t{}\t{}\t{}\n".format(sampleID, read.id, readStr, readInfp[read.id], BarcodeSequence)
 
           
         
